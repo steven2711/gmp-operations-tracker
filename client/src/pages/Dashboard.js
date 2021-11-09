3cc3950b7236c69ebc0c13ab444cd63e320eaf3f
Steven Watson <swatson.emu@gmail.com>
2021-11-09 12:43:41 -0600

    Added layout component to dashboard page

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Layout from "../layout";
 
 const Dashboard = () => {
   return (
-    <div>
+    <Layout>
       <h1>Dashboard page</h1>
-    </div>
+    </Layout>
   );
 };
 

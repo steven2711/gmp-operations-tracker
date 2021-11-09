0c43e12ffbb6709e4a5a46da10acf2fd47f75ba7
Steven Watson <swatson.emu@gmail.com>
2021-11-09 11:49:17 -0600

    Boilerplate for pages.

---
+++ client/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../layout";
+
+const Home = () => {
+  return (
+    <Layout>
+      <h1>other stuff</h1>
+    </Layout>
+  );
+};
+
+export default Home;

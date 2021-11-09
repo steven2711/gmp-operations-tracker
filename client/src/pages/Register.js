caccfca3d74462bf8b3012cf9587cf29bb1df3da
Steven Watson <swatson.emu@gmail.com>
2021-11-09 12:43:13 -0600

    Added register page.

---
+++ client/src/pages/Register.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../layout";
+
+const Register = () => {
+  return (
+    <Layout>
+      <h1>Register page</h1>
+    </Layout>
+  );
+};
+
+export default Register;
--- client/src/routing/Router.js
+++ client/src/routing/Router.js
@@ -1,13 +1,15 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "../pages/Home";
 import Dashboard from "../pages/Dashboard";
+import Register from "../pages/Register";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route exact path="/about" component={Dashboard} />
+        <Route exact path="/dashboard" component={Dashboard} />
+        <Route exact path="/register" component={Register} />
       </Switch>
     </BrowserRouter>
   );

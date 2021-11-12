51fa90f6a614c310f80799ae8917bc5d542140d6
Steven Watson <swatson.emu@gmail.com>
2021-11-12 10:01:51 -0600

    Added pointer to production build

--- server.js
+++ server.js
@@ -19,6 +19,17 @@ db();
 app.use("/api/v1/users", require("./routes/users"));
 app.use("/api/v1/auth", require("./routes/auth"));
 
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
+
 // Server
 
 const PORT = process.env.PORT || 5000;

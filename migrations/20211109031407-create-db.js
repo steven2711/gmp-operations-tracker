244c1d36a71affc6eeac16beb526da47116a6736
Steven Watson <swatson.emu@gmail.com>
2021-11-13 11:10:54 -0600

    Tinkering with user model

--- migrations/20211109031407-create-db.js
+++ migrations/20211109031407-create-db.js
@@ -17,6 +17,12 @@ module.exports = {
       email: {
         type: Sequelize.STRING,
       },
+      password: {
+        type: Sequelize.STRING,
+      },
+      role: {
+        type: Sequelize.STRING,
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE,

3e83a68812766de39faaa5c81d841a9b509a93cc
Steven Watson <swatson.emu@gmail.com>
2021-11-13 11:08:11 -0600

    Auth route edit

--- controllers/auth.js
+++ controllers/auth.js
@@ -9,16 +9,14 @@ exports.getUserInfo = async (req, res) => {
   res.status(200).json({ msg: "Get user info" });
 };
 
-// @route POST api/v1/auth
+// @route POST api/v1/auth/login
 // @desc Auth user and get token
 // @access Public
 
 exports.loginUser = async (req, res) => {
   const { email, password } = req.body;
 
-  res
-    .status(200)
-    .json({
-      msg: `Login user with email ${email} and password ${password} successful`,
-    });
+  res.status(200).json({
+    msg: `Login user with email ${email} and password ${password} successful`,
+  });
 };
